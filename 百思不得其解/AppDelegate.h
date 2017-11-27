9efe834b72735564a2c5c58252398ebbae58a115
longc-chen <longc-chen@users.noreply.github.com>
2017-11-27 20:45:48 +0800

    Initial Commit

---
+++ 百思不得其解/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得其解
+//
+//  Created by LongCh on 2017/11/27.
+//  Copyright © 2017年 LongCh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
